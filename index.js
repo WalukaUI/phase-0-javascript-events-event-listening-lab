a9b737ac070d48d69a8fb41575de1b68a7288151
WalukaUI <cwaluka@yahoo.com>
2021-03-22 16:25:54 -0700

    Listners

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+
+
 function addingEventListener() {
+    
+    const input = document.getElementById('input');
+    const OtherDiv = document.getElementById('newmsg');
+    
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    function eventListener() {
+        alert("Hee hee, that tickles!");
+      }
+    
+    input.addEventListener('click', clickAlert);
+    OtherDiv.addEventListener("click",eventListener);
+    clickAlert();
+    
+
 }
+
+
